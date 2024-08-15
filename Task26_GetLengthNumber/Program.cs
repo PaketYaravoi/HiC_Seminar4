de28d7c1e6f6500e7811f99ee01da2366c13653e
PaketYaravoi <roma.klimov10.94@gmail.com>
2024-08-15 11:37:18 +1100

    create tesk 26

--- Task26_GetLengthNumber/Program.cs
+++ Task26_GetLengthNumber/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе:
+// 456 -> 3 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 0;
+
+        Console.Write("Please enter integer: ");
+        while (!int.TryParse(Console.ReadLine(), out number))
+        { 
+            Console.WriteLine("Input error!");
+            Console.Write("Please restart enter: ");
+        }
+
+        string strFromN = GetStrFromNumber(number);
+        Console.WriteLine(GetLenghthNumber(strFromN));
+    }
+
+    static string GetStrFromNumber(int number)
+    { 
+        string str = String.Empty;
+        return str = number.ToString();
+    }
+
+    static int GetLenghthNumber(string str)
+    {   
+        int length = str.Length;
+        return length;
+        
+    }
+
+}
